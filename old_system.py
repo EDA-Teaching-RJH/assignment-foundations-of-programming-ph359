f35bf408b9d60be02b15fe36a3b81a144e3a6880
Philip <ph359@kent.ac.uk>
2026-02-06 13:53:25 +0000

    line28_Bug_1

--- old_system.py
+++ old_system.py
@@ -25,7 +25,7 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt = "1":  
+        if opt == "1": #Sytax Error: single equals (=) symbol instead of two (==). the if statement was being assigned to '1', instead of being equals to '1'.
             print("Current Crew List:")
             
             for i in range(10):
