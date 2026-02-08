a728ab6abefc936ae19335e0a6577abb696a3e70
Philip <ph359@kent.ac.uk>
2026-02-08 21:18:35 +0000

    bug 6: opt == 1

--- old_system.py
+++ old_system.py
@@ -25,7 +25,7 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt = "1":  
+        if opt == "1": #Added == to made opt equals to 1
             print("Current Crew List:")
             
             for i in range(10):
