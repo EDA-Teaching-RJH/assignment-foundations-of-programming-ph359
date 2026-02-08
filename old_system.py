4a16aec784d516b956956f37e28ee4e09ef1730b
Philip <ph359@kent.ac.uk>
2026-02-08 21:47:26 +0000

    Bug 10: .title()

--- old_system.py
+++ old_system.py
@@ -44,7 +44,7 @@ def run_system_monolith():
             print("Crew member added.")
             
         elif opt == "3":
-            rem = input("Name to remove: ")
+            rem = input("Name to remove: ").title() #First letter is capatalised.
            
             idx = n.index(rem)
             n.pop(idx)
