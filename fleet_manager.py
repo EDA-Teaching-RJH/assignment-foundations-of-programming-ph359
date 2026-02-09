8c0a5e4e5f058c43663b6b63f0c682bd5c8073ca
Philip <ph359@kent.ac.uk>
2026-02-09 01:44:00 +0000

    Added core functions

---
+++ fleet_manager.py
@@ -0,0 +1,60 @@
+def init_database(): #1
+
+
+
+def display_menu(): #2
+
+
+
+
+def add_member(): #3
+
+
+
+
+def remove_member(): #4
+
+
+
+
+def update_rank(): #5
+
+
+
+
+def display_roster(): #6
+
+
+
+
+def search_crew(): #7
+
+
+
+
+def filter_by_division(): #8
+
+
+
+
+def calculate_payroll(): #9
+
+
+
+
+def count_officer(): #10
+
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+main()
