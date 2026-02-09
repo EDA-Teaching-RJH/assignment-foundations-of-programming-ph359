4fccb7bcce799c4d9260864c70deb40a2c614bf6
Philip <ph359@kent.ac.uk>
2026-02-09 19:44:33 +0000

    Added login in fn2

--- fleet_manager.py
+++ fleet_manager.py
@@ -3,65 +3,24 @@ def init_database(): #1
     ranks = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Ensign"]
     divs = ["Command", "Science", "Operations", "Operations", "Operations"]
     ids = ["001", "002", "003", "004", "005"]
-
     return names, ranks, divs, ids
+init_database()
+
+def display_menu():
+    print("\n---Initilising Fleet Manager---")
+    print("   Welcome to Fleet Manager   ")
+
+    print("\n=========================")
+    login = input("Enter Full Name: ").title().strip()
+    print(f"Logged in as: {login}")
+    print("=========================") 
+
+    
 
 
 
 
-def display_menu(): #2
+display_menu()
 
 
 
-
-def add_member(): #3
-
-
-
-
-def remove_member(): #4
-
-
-
-
-def update_rank(): #5
-
-
-
-
-def display_roster(): #6
-
-
-
-
-def search_crew(): #7
-
-
-
-
-def filter_by_division(): #8
-
-
-
-
-def calculate_payroll(): #9
-
-
-
-
-def count_officer(): #10
-
-
-
-
-def main(): 
-
-
-
-
-
-
-
-
-
-main()
