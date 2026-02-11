4e9d23843f394fb2afbf15c4020c940ea4db50c7
Philip <ph359@kent.ac.uk>
2026-02-11 14:17:49 +0000

    Added functionality to fn3, set inputs, valid ranks/divs, and if statments for paramenters. inputs now sotred to fn1

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,17 +1,17 @@
 def init_database(): #1
-    names = ["Pike", "Spock", "Scott", "Uhura", "Chekov"]
-    ranks = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Ensign"]
+    names = ["Picard", "Riker", "Data", "Worf", "Crusher"]
+    ranks = ["Captain", "Commander", "Lieutenant Commander", "Lieutenant", "Acting Ensign"]
     divs = ["Command", "Science", "Operations", "Operations", "Operations"]
     ids = ["001", "002", "003", "004", "005"]
     return names, ranks, divs, ids
 
 init_database()
 
-def display_menu():
+def display_menu(): #2
     print("\n---Initilising Fleet Manager---")
     print("   Welcome to Fleet Manager   ")
     
-    print("\n=========================")
+    print("=========================")
     
     login = input("Enter Full Name: ").title().strip()
     print(f"Logged in as: {login}")
@@ -19,21 +19,56 @@ def display_menu():
     print("=========================") 
 
     print("\n-----Options-----")
-    print("1) Add Members")
-    print("2) Remove Members")
-    print("3) Update Ranks")
-    print("4) Display Roster")
-    print("5) Search Crew")
-    print("6) Filter by Diviosn")
-    print("7) Calculate Payroll")
-    print("8) Count Officers")
-    print("9) Exit & logout")
+    print("1. Add Members")
+    print("2. Remove Members")
+    print("3. Update Ranks")
+    print("4. Display Roster")
+    print("5. Search Crew")
+    print("6. Filter by Diviosn")
+    print("7. Calculate Payroll")
+    print("8. Count Officers")
+    print("9. Exit & logout")
 
     choice = input("\nChoose an option: ")
     return choice
 
 display_menu()
 
-def add_members():
+def add_member(names, ranks, divs, ids):
+    name = input("Enter name: ").title().strip()
+    rank = input("Enter rank: ").title().strip()
+    division = input("Enter Divison (Command/Operations/Sciences): ").title().strip()
+    new_id = input(int("Enter crew ID: ")).strip()
 
-add_members()
+    valid_ranks = [
+        "Captain", "Commander", "Lieutenant Commander", 
+        "Lieutenant", "Acting Ensign"
+    ]
+    valid_divisions = [
+        "Command","Operations","Sciences"
+        ]
+
+    if rank not in valid_ranks:
+        print("Invalid Rank. Please choose from; Captain, Commander," \
+        " Lieutentant Commander, Lieutenant, Acting Ensign")
+        return
+    
+    if division not in valid_divisions:
+        print("invalid Divison. Choose from; Command, Operations, Sciences")
+        return
+    
+    if new_id in ids:
+        print("ID already exists.")
+        return
+    
+    names.append(name)
+    ranks.append(rank)
+    divs.append(division)
+    ids.append(new_id)
+
+    print(f"{name} added to crew")
+
+add_member()
+                
+
+
